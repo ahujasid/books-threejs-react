294d7fd404efa7497a57c86af0970ae9b89b206c
ahujasid <ahujasid@gmail.com>
2024-11-20 16:30:37 +0530

    made desktop animation more evident

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -40,9 +40,9 @@ const Book = ({
       if (!bookContainerRef) return;
       else{
         if(!isMobile){
-          animationTime += 0.05;
-          bookContainerRef.rotation.y = Math.PI * 0.05 + Math.sin(animationTime * 0.5) * 0.05;
-          bookContainerRef.position.y = Math.sin(animationTime * 0.7) * 0.05;
+          animationTime += 0.07;
+          bookContainerRef.rotation.y = Math.PI * 0.05 + Math.sin(animationTime * 0.5) * 0.07;
+          bookContainerRef.position.y = Math.sin(animationTime * 0.7) * 0.07;
         }
         else{
           animationTime += 0.08;
